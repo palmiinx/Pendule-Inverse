d510bcf74be937ebb402621b95e0d10911964bdd
palmiinx <clement.etore@depinfonancy.net>
2020-03-16 12:13:24 +0100

    Voiture bouge au centre

--- voiture.py
+++ voiture.py
@@ -1,31 +1,35 @@
 from tkinter import *
- 
-def deplacement():
-    canvas.move(W_image,0,5)
-   
-    #On repete cette fonction
-    root.after(40,deplacement)
- 
+
+#Fonctions
+
+def deplacer(can, rect, x, sens) :
+    x = x + sens*5
+    if x == 40 :
+        sens=-1
+    if x == -40 :      
+        sens=1
+    can.move(rect, sens*5, 0)
+    can.after(50, deplacer, can, rect, x, sens)
+
+def creer_voiture():
+    imgfile = 'Voiture.png'
+    img = PhotoImage(file = imgfile)
+
+    gifsdict={}
+    gifsdict[imgfile] = img
+
+    picture = img.subsample(5, 5) #reduire echelle de l'image
+    return(picture)
+
+#Main
+
 root = Tk()
 
-#chemin d'acces a l'image:
-imgfile = 'Voiture.png'
-# Utilisation d'un dictionnaire pour conserver une reference:
-gifsdict={}
+can = Canvas(root, width=1200, height=600)
+can.grid()
 
-#Creation de l'image:
-img = PhotoImage(file = imgfile)
-gifsdict[imgfile] = img
-
-#Creation du canvas:
-canvas=Canvas(root,width = 500, height = 400 , bd=0, bg="white")
-canvas.pack(padx=10,pady=10)
-
-#Un exemple de reduction de l'image:
-img_2 = img.subsample(2, 2)
-
-#On cree le Widget image dans le canvas:
-#NW=Nord West, le coin haut guche de l'image sera positionne a (10,10):
-W_image=canvas.create_image(50,50,anchor=NW,image=img_2)
+picture=creer_voiture()
+voiture=can.create_image(500,470,anchor=NW,image=picture)
+deplacer(can, voiture, 0, 1)
 
 root.mainloop()
